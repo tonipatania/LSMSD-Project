567529b944c754c3454b9f7e96b7204f1df5640b
antonino <antopata99@gmail.com>
2026-07-28 12:17:11 +0200

    search games queries

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/repository/MongoDBAggregation/GameRepositoryCustom.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/repository/MongoDBAggregation/GameRepositoryCustom.java
@@ -3,6 +3,8 @@ package it.unipi.lsmsd.gamehub.repository.MongoDBAggregation;
 import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation;
 import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation2;
 import it.unipi.lsmsd.gamehub.model.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -12,4 +14,8 @@ public interface GameRepositoryCustom {
     List<GameDTOAggregation> findAggregation();
 
     List<GameDTOAggregation2> findAggregation4();
+
+    Page<Game> searchGames(String name, List<String> genres, Integer avgScore, Pageable pageable);
+
+    List<String> findDistinctGenres();
 }
